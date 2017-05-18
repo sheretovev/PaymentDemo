7d1d54ff3818d58bd60fffc27f14bd73e573eecb
Eugene Sheretov <eugene.sheretov@iquality.nl>
2017-05-19 00:18:07 +0200

    fix

--- Deploy/Publish-WebApi-dev.cmd
+++ Deploy/Publish-WebApi-dev.cmd
@@ -3,4 +3,4 @@ echo replace appsettings
 C:\Windows\system32\inetsrv\appcmd stop apppool PaymentDemo
 xcopy ".\*" "c:\inetpub\PaymentDemo\" /s /e /y /c
 C:\Windows\system32\inetsrv\appcmd start apppool PaymentDemo
-cd ..\..\..\..\..\..\..\Deploy
+cd ..\..\..\..\Deploy
